4e93b32e32030c2ea78fd2bcba3598a606c3eb82
Isaac Hinman <isaac@isaachinman.com>
2017-04-18 14:26:32 +0200

    Update upload legacy code

--- src/utils/upload/UploadImage.js
+++ src/utils/upload/UploadImage.js
@@ -21,7 +21,6 @@ var uploadImage = function(image_file,image_key,options,callback){
     case 'image/jpeg':
     case 'image/pjpeg': type = 'jpg'; break;
     case 'image/png': type = 'png'; break;
-    case 'application/pdf': type = 'pdf'; break;
     default:
       console.log("Error! Not supported image type:",image_file);
       if(callback) callback("Not supported image type.");
@@ -33,126 +32,77 @@ var uploadImage = function(image_file,image_key,options,callback){
   var crop_width = options ? options.crop ? options.crop.width : width : width;
   var crop_height = 0;
   var exifData;
-
-  if (type === 'pdf') {
-    var params = {
-      ACL: 'public-read',
-      Bucket: config.s3,
-      Key: image_key,
-      Body: image_file.buffer,
-      ContentType: image_file.mimetypem,
-      CacheControl: 'max-age=1209600'
-    }
-    s3.putObject(params, function(err, data) {
-        if(err) console.log(err);
-        if(callback) callback(err);
-    });
-  }
-
-  if (type == "jpg") {
+  if(type == "jpg"){
     exifData = exif.create(image_file.buffer).parse();
   }
 
-  if (type != 'pdf') {
-    lwip.open(image_file.buffer,type,function(err, image){
-      var inverted = false;
-      if(!height){
-        height = image.height()*(width/image.width());
-        crop_height = options ? options.crop ? options.crop.height : height : height;
+  lwip.open(image_file.buffer,type,function(err, image){
+    var inverted = false;
+    if(!height){
+      height = image.height()*(width/image.width());
+      crop_height = options ? options.crop ? options.crop.height : height : height;
+    }
+    var processed = image.batch();
+    if(exifData){
+      switch( exifData.tags.Orientation ) {
+        case 2:
+        processed = image.batch().flip('x'); // top-right - flip horizontal
+        break;
+        case 3:
+        processed = image.batch().rotate(180); // bottom-right - rotate 180
+        break;
+        case 4:
+        processed = image.batch().flip('y'); // bottom-left - flip vertically
+        break;
+        case 5:
+        inverted = true;
+        processed = image.batch().rotate(90).flip('x'); // left-top - rotate 90 and flip horizontal
+        break;
+        case 6:
+        inverted = true;
+        processed = image.batch().rotate(90); // right-top - rotate 90
+        break;
+        case 7:
+        inverted = true;
+        processed = image.batch().rotate(270).flip('x'); // right-bottom - rotate 270 and flip horizontal
+        break;
+        case 8:
+        inverted = true;
+        processed = image.batch().rotate(270); // left-bottom - rotate 270
+        break;
       }
-      var processed = image.batch();
-      if(exifData){
-        switch( exifData.tags.Orientation ) {
-          case 2:
-          processed = image.batch().flip('x'); // top-right - flip horizontal
-          break;
-          case 3:
-          processed = image.batch().rotate(180); // bottom-right - rotate 180
-          break;
-          case 4:
-          processed = image.batch().flip('y'); // bottom-left - flip vertically
-          break;
-          case 5:
-          inverted = true;
-          processed = image.batch().rotate(90).flip('x'); // left-top - rotate 90 and flip horizontal
-          break;
-          case 6:
-          inverted = true;
-          processed = image.batch().rotate(90); // right-top - rotate 90
-          break;
-          case 7:
-          inverted = true;
-          processed = image.batch().rotate(270).flip('x'); // right-bottom - rotate 270 and flip horizontal
-          break;
-          case 8:
-          inverted = true;
-          processed = image.batch().rotate(270); // left-bottom - rotate 270
-          break;
+    }
+    if(inverted){
+      var newone = height;
+      height = width;
+      width = newone;
+      var newone = crop_height;
+      crop_height = crop_width;
+      crop_width = newone;
+    }
+    processed.resize(width,height)
+      .crop(crop_width,crop_height)
+      .toBuffer('jpg',{quality:90}, function(err,buffer){
+        if(err){
+          console.log(err);
+          if(callback) callback(err);
         }
-      }
-      if(inverted){
-        var newone = height;
-        height = width;
-        width = newone;
-        var newone = crop_height;
-        crop_height = crop_width;
-        crop_width = newone;
-      }
-
-      console.log('OPTIONS: ', options)
-      if (options.customCrop) {
-
-        processed.crop(options.cropData.x, options.cropData.y, options.cropData.x + options.cropData.width, options.cropData.y + options.cropData.height)
-          .resize(width,height)
-          .toBuffer('jpg',{quality:90}, function(err,buffer){
-            if(err){
-              console.log(err);
+        else{
+          var params = {
+            ACL: 'public-read',
+            Bucket: config.s3,
+            Key: image_key,
+            Body: buffer,
+            ContentType: image_file.mimetypem,
+            CacheControl: 'max-age=1209600'
+          }
+          s3.putObject(params, function(err, data) {
+              if(err) console.log(err);
               if(callback) callback(err);
-            }
-            else{
-              var params = {
-                ACL: 'public-read',
-                Bucket: config.s3,
-                Key: image_key,
-                Body: buffer,
-                ContentType: image_file.mimetypem,
-                CacheControl: 'max-age=1209600'
-              }
-              s3.putObject(params, function(err, data) {
-                  if(err) console.log(err);
-                  if(callback) callback(err);
-              });
-            }
-          });
-
-      } else {
-        processed.resize(width,height)
-          .crop(crop_width,crop_height)
-          .toBuffer('jpg',{quality:90}, function(err,buffer){
-            if(err){
-              console.log(err);
-              if(callback) callback(err);
-            }
-            else{
-              var params = {
-                ACL: 'public-read',
-                Bucket: config.s3,
-                Key: image_key,
-                Body: buffer,
-                ContentType: image_file.mimetypem,
-                CacheControl: 'max-age=1209600'
-              }
-              s3.putObject(params, function(err, data) {
-                  if(err) console.log(err);
-                  if(callback) callback(err);
-              });
-            }
-          });
-      }
-
-    });
-  }
-
+           });
+        }
+      });
+  });
 }
 
 module.exports = uploadImage;
